f341282b8c2e78871b29acef6b74edf305aedfc6
Romel Rey Silva <sromelrey@gmail.com>
2025-08-24 21:19:14 +0800

    chore(export components): add core components to exports

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
 // index.ts
-export * from "@/components/custom/form-fields";
+export * from "@/components/core/form-fields";
+export * from "@/components/core/data-table";
+export * from "@/components/core/table";
